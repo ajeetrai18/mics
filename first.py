20d8ee50daef9cd22d14e36458dedac1e54d6b99
Ajeet <ajeetrai18@gmail.com>
2021-08-02 16:30:28 +0530

    commi2 from testbranch

--- first.py
+++ first.py
@@ -1,6 +1,8 @@
 number = input("enter the number: ")
 print("Will not work for large numbers")
 print("Will not work for Decimal numbers")
+print("Will not work for strings")
+
 if int(number) % 2 == 0:
     print("Number is even")
 else:
